f8216633a1c8cdd39e3b5163ec87c44e8b88d98c
Dudemanguy <random342@airmail.cc>
2023-06-17 16:31:45 -0500

    convert mainwindow.h to spaces
    Was mistakenly left out of the initial commit. Also make the define
    MAINWINDOW_H instead.

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -1,37 +1,37 @@
-#ifndef WINDOW_H
-#define WINDOW_H
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
 
 #include <QLineEdit>
 #include <QMainWindow>
 #include <sqlite3.h>
 
 struct mainSettings {
-	QAction *importSettings;
+    QAction *importSettings;
 };
 
 class MainWindow : public QMainWindow {
-	public:
-		explicit MainWindow(QWidget *parent = 0);
-		mainSettings *settings;
-		sqlite3 *database;
-		QStringList entries;
-		QAction *importSettings;
-		QListView *listView;
-		QStringListModel *model;
-		QLineEdit *searchBox;
-		QCheckBox *searchNames;
-		QDialog *tagDialog;
-		QLineEdit *tagEdit;
-	private slots:
-		void addDirectory(bool recursive);
-		void addFiles();
-		void closeEvent(QCloseEvent *event);
-		void openFiles();
-		void removeFiles();
-		void tagFiles();
-		void updateEntries(const QString str);
-		void updateNameCheck(bool checked);
-		void updateTags(bool add);
+    public:
+        explicit MainWindow(QWidget *parent = 0);
+        mainSettings *settings;
+        sqlite3 *database;
+        QStringList entries;
+        QAction *importSettings;
+        QListView *listView;
+        QStringListModel *model;
+        QLineEdit *searchBox;
+        QCheckBox *searchNames;
+        QDialog *tagDialog;
+        QLineEdit *tagEdit;
+    private slots:
+        void addDirectory(bool recursive);
+        void addFiles();
+        void closeEvent(QCloseEvent *event);
+        void openFiles();
+        void removeFiles();
+        void tagFiles();
+        void updateEntries(const QString str);
+        void updateNameCheck(bool checked);
+        void updateTags(bool add);
 };
 
 #endif
